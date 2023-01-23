e4ccbbb9247f7ee7bcce97de406a17cf831888a5
WeilSimon <weils@catlin.edu>
2023-01-22 18:15:15 -0800

    feat: added code for outlined methods for the arm

--- src/main/java/org/team1540/robot2023/Constants.java
+++ src/main/java/org/team1540/robot2023/Constants.java
@@ -145,6 +145,7 @@ public final class Constants {
         public static final int TELESCOPE_ID = 12;
         public static final int CANCODER_ID = 13;
 
+        public static final double PIVOT_FF = 0;
         public static final double PIVOT_KP = 0;
         public static final double PIVOT_KI = 0;
         public static final double PIVOT_KD = 0;
@@ -154,6 +155,7 @@ public final class Constants {
         // The distance of the pivot from the edge of the frame perimeter
         public static final double PIVOT_DISTANCE = 12;
 
+        public static final double TELESCOPE_FF = 0;
         public static final double TELESCOPE_KP = 0;
         public static final double TELESCOPE_KI = 0;
         public static final double TELESCOPE_KD = 0;
