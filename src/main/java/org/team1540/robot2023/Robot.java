81fe8ac6d505fd7d883af8fb4590ab3d93e25e7c
Zach R <zarutman+git@gmail.com>
2023-01-29 11:31:14 -0800

    feat: turn off LEDs on startup

--- src/main/java/org/team1540/robot2023/Robot.java
+++ src/main/java/org/team1540/robot2023/Robot.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import org.team1540.robot2023.utils.Limelight;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -34,6 +35,7 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void robotInit() {
+        Limelight.setLedState(Limelight.LEDMode.OFF);
         ctreConfigs = new CTREConfigs();
         // Instantiate our RobotContainer. This will perform all our button bindings,
         // and put our autonomous chooser on the dashboard.
