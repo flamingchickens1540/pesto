b86d6f5ca54ecb7a9e9b3f7ef09fd31e6170969a
Zach R <zarutman+git@gmail.com>
2023-02-25 23:29:59 -0800

    feat: add back set arm position

---
+++ src/main/java/org/team1540/robot2023/commands/arm/SetArmPosition.java
@@ -0,0 +1,13 @@
+package org.team1540.robot2023.commands.arm;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import org.team1540.robot2023.utils.ArmState;
+
+public class SetArmPosition extends SequentialCommandGroup {
+    public SetArmPosition(Arm arm, ArmState setpoint) {
+        addCommands(
+                new PivotCommand(arm,setpoint.getRotation2d()),
+                new ExtensionCommand(arm,setpoint.getExtension())
+        );
+    }
+}
