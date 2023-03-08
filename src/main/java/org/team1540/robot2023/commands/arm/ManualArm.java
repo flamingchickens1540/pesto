8ed12fcc052e8b5faf9def78479bc3f2ef18353b
Alvin Zhang <41vin2h4n9@gmail.com>
2023-03-07 16:19:38 -0800

    fix: better holding logic and transitioning between manual and auto

--- src/main/java/org/team1540/robot2023/commands/arm/ManualArm.java
+++ src/main/java/org/team1540/robot2023/commands/arm/ManualArm.java
@@ -12,6 +12,8 @@ public class ManualArm extends CommandBase {
     private final double deadzone = 0.1;
     private boolean isHoldingRotation;
     private boolean isHoldingExtension;
+    private boolean startedManualExtension;
+    private boolean startedManualPivot;
 
     public ManualArm(Arm arm, CommandXboxController controller){
         this.arm = arm;
@@ -23,27 +25,34 @@ public class ManualArm extends CommandBase {
     public void initialize() {
         isHoldingRotation = false;
         isHoldingExtension = false;
+        startedManualExtension = false;
+        startedManualPivot = false;
     }
 
     @Override
     public void execute() {
         double pivotInput = -controller.getLeftY();
-        if (Math.abs(pivotInput) <= deadzone && !isHoldingRotation) {
-            isHoldingRotation = true;
-            arm.setRotation(arm.getArmState().getRotation2d());
-        } else if (Math.abs(pivotInput) >= deadzone) {
-            isHoldingRotation = false;
-            arm.setRotatingSpeed(Math.pow(pivotInput, 3) * (1 + ((-1/ArmConstants.TELESCOPE_FORWARD_LIMIT)*pivotInput))); // TODO: 2/11/2023 Check angles here
+        if (!startedManualPivot && Math.abs(pivotInput) >= deadzone) startedManualPivot = true;
+        if (startedManualPivot) {
+            if (Math.abs(pivotInput) <= deadzone && !isHoldingRotation) {
+                isHoldingRotation = true;
+                arm.setRotation(arm.getArmState().getRotation2d());
+            } else if (Math.abs(pivotInput) >= deadzone) {
+                isHoldingRotation = false;
+                arm.setRotatingSpeed(Math.pow(pivotInput, 3) * (1 + ((-1 / ArmConstants.TELESCOPE_FORWARD_LIMIT) * pivotInput))); // TODO: 2/11/2023 Check angles here
+            }
         }
-
-        double limitedExtensionInput = slewRateLimiter.calculate(controller.getLeftTriggerAxis() - controller.getRightTriggerAxis());
-        if(Math.abs(limitedExtensionInput) <= deadzone && !isHoldingExtension){
-            isHoldingExtension = true;
-            arm.setExtension(arm.getArmState().getExtension());
-        }
-        else if (Math.abs(limitedExtensionInput) >= deadzone){
-            isHoldingExtension = false;
-            arm.setExtendingSpeed(limitedExtensionInput);
+        double extensionInput = controller.getLeftTriggerAxis() - controller.getRightTriggerAxis();
+        if (!startedManualExtension && Math.abs(extensionInput) >= deadzone) startedManualExtension = true;
+        double limitedExtensionInput = Math.abs(extensionInput) < deadzone ? 0 : slewRateLimiter.calculate(extensionInput);
+        if (startedManualExtension) {
+            if (Math.abs(limitedExtensionInput) <= deadzone && !isHoldingExtension) {
+                isHoldingExtension = true;
+                arm.setExtension(arm.getArmState().getExtension());
+            } else if (Math.abs(limitedExtensionInput) >= deadzone) {
+                isHoldingExtension = false;
+                arm.setExtendingSpeed(limitedExtensionInput);
+            }
         }
     }
 
