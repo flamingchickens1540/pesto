e3fe89691313d0be1872083c5a021780e7562fb9
Simon Weil <weils@catlin.edu>
2023-03-13 22:58:28 -0700

    feat: update gridscore with SetArmPosition and implement ResetArmPositionCommand

--- src/main/java/org/team1540/robot2023/commands/arm/ResetArmPositionCommand.java
+++ src/main/java/org/team1540/robot2023/commands/arm/ResetArmPositionCommand.java
@@ -1,16 +1,67 @@
 package org.team1540.robot2023.commands.arm;
 
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import org.team1540.robot2023.Constants;
+import org.team1540.robot2023.utils.ArmState;
+import org.team1540.robot2023.utils.AverageFilter;
 
-public class ResetArmPositionCommand extends SequentialCommandGroup {
-    public ResetArmPositionCommand(Arm arm) {
-        addCommands(
-                new InstantCommand(arm::stopAll).withName("StopArm"),
-                new RetractAndPivotCommand(arm, Rotation2d.fromDegrees(0)),
-                new InstantCommand(arm::resetAngle).withName("ResetArmAngle"),
-                new RetractAndPivotCommand(arm, Rotation2d.fromDegrees(0))
+public class ResetArmPositionCommand extends CommandBase {
+
+    Arm arm;
+    ArmState setpoint;
+
+    private final AverageFilter extensionFilter = new AverageFilter(10);
+    private final AverageFilter rotationFilter = new AverageFilter(5);
+    private final double extensionThreshold = 0.25;
+    private final double rotationThreshold = 0.5;
+    private boolean isRotating;
+    private long pivotStartTime;
+    private double extensionDelay;
+
+    public ResetArmPositionCommand(Arm arm){
+        this.arm = arm;
+        this.setpoint = ArmState.fromRotationExtension(Rotation2d.fromDegrees(0), Constants.ArmConstants.ARM_BASE_LENGTH);
+        addRequirements(arm);
+    }
+
+    @Override
+    public void initialize() {
+        System.out.println(arm.timeToExtension(Constants.ArmConstants.ARM_BASE_LENGTH));
+        pivotStartTime = (long) (System.currentTimeMillis() + arm.timeToExtension(setpoint.getExtension())/5);
+        arm.setExtension(setpoint.getExtension());
+        arm.setRotation(arm.getArmState().getRotation2d());
+        isRotating = false;
+        extensionFilter.clear();
+        rotationFilter.clear();
+    }
+
+    @Override
+    public void execute() {
+        if(!isRotating){
+//            System.out.println("Extension Start: " + extensionStartTime + " Now: " + System.currentTimeMillis());
+            System.out.println(pivotStartTime - System.currentTimeMillis());
+            if(System.currentTimeMillis() >= pivotStartTime){
+                isRotating = true;
+                arm.setRotation(setpoint.getRotation2d());
+            }
+        }
+        extensionFilter.add(Math.abs(setpoint.getExtension() - arm.getArmState().getExtension()));
+        rotationFilter.add(Math.abs(setpoint.getRotation2d().getDegrees() - arm.getArmState().getRotation2d().getDegrees()));
+    }
+
+    @Override
+    public boolean isFinished() {
+        return (
+                ((setpoint.getExtension() <= Constants.ArmConstants.ARM_BASE_LENGTH && arm.getLimitSwitch()) ||
+                        extensionFilter.getAverage() < extensionThreshold) &&
+                        (rotationFilter.getAverage() < rotationThreshold &&
+                                Math.abs(arm.getArmState().getRotation2d().getDegrees() - setpoint.getRotation2d().getDegrees()) < rotationThreshold)
         );
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        arm.stopAll();
     }
 }
