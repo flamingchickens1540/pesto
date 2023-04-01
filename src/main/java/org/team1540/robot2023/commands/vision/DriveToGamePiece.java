bc3df23f8b1433f081508e222fc2e6816c3db077
Sana Shah <54493565+sanashah007@users.noreply.github.com>
2023-04-01 10:24:56 -0700

    feat: It drives omg

--- src/main/java/org/team1540/robot2023/commands/vision/DriveToGamePiece.java
+++ src/main/java/org/team1540/robot2023/commands/vision/DriveToGamePiece.java
@@ -3,35 +3,40 @@ package org.team1540.robot2023.commands.vision;
 import com.pathplanner.lib.PathPoint;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.ProxyCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
 import java.util.function.DoubleSupplier;
 
 import org.team1540.robot2023.commands.auto.AutoDrive;
 import org.team1540.robot2023.commands.drivetrain.Drivetrain;
 
-public class DriveToGamePiece extends CommandBase{
+public class DriveToGamePiece extends SequentialCommandGroup {
     Drivetrain drivetrain;     
-    DoubleSupplier hypotenuse; 
-    double hypotenuseOutput = hypotenuse.getAsDouble(); 
+    DoubleSupplier hypotenuseSupplier; 
+   
 
-    public DriveToGamePiece(Drivetrain drivetrain, DoubleSupplier hypotenus){
+    public DriveToGamePiece(Drivetrain drivetrain, DoubleSupplier hypotenuseSupplier){
         this.drivetrain = drivetrain; 
-        this.hypotenuse = hypotenus; 
+        this.hypotenuseSupplier = hypotenuseSupplier; 
+        addCommands(
+            new ProxyCommand(
+                AutoDrive.driveToPoints(drivetrain, 0.5, 1, calculateEndPoint())
+            )
+        );
+        addRequirements(drivetrain);
     }
     
      /**
      * @param Hypotenuse Hypotenuse to game piece in meters 
      * @return PathPoint of gamepiece 
      */
-    public PathPoint calculateLength() {
-       double length = Math.sin(drivetrain.getYaw().getDegrees()) * hypotenuseOutput; 
+    public PathPoint calculateEndPoint() {
+       double hypotenuseOutput = hypotenuseSupplier.getAsDouble(); 
+       double height = Math.sin(drivetrain.getYaw().getDegrees()) * hypotenuseOutput; 
        double width = Math.cos(drivetrain.getYaw().getDegrees()) * hypotenuseOutput; 
        Translation2d currentTranslation = drivetrain.getPose().getTranslation(); 
-       PathPoint point = new PathPoint(currentTranslation.plus(new Translation2d(width, length)), drivetrain.getYaw()); 
+       PathPoint point = new PathPoint(currentTranslation.plus(new Translation2d(width, height)), drivetrain.getYaw()); 
        return point; 
-    }
-
-    @Override
-    public void execute() {
-        calculateLength(); 
     }
 }
