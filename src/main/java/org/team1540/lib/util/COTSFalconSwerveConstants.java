8d3ad7fb95293e72a44254ae91ff405860c462d8
Sana Shah <54493565+sanashah007@users.noreply.github.com>
2023-04-11 16:17:39 -0700

    fix: auto adjustments with new tread

--- src/main/java/org/team1540/lib/util/COTSFalconSwerveConstants.java
+++ src/main/java/org/team1540/lib/util/COTSFalconSwerveConstants.java
@@ -32,7 +32,7 @@ public class COTSFalconSwerveConstants {
     
     /** Swerve Drive Specialties - MK3 Module*/
     public static COTSFalconSwerveConstants SDSMK3(double driveGearRatio){
-        double wheelDiameter = Units.inchesToMeters(4.0);
+        double wheelDiameter = Units.inchesToMeters(4.0*0.6);
  
         /** 12.8 : 1 */
         double angleGearRatio = (12.8 / 1.0);
